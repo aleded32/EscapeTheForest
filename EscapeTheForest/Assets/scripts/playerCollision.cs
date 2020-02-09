72f4086993eba70602a635106ee9b25ced218068
aleded32 <alededwards32@gmail.com>
2020-02-09 07:08:34 +0000

    V1.2
    enemy collision and score added

---
+++ EscapeTheForest/Assets/scripts/playerCollision.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerCollision : MonoBehaviour {
+
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Debug.Log(1);
+            SceneManager.LoadScene("GameScreen");
+
+        }
+
+    }
+
+
+
+}
