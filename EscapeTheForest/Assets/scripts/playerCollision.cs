aefbb31a097ccf05c2293442b384be7d78a1a00a
aleded32 <alededwards32@gmail.com>
2020-03-16 15:28:57 +0000

    V1.5
    obstacle collision now added with effects

--- EscapeTheForest/Assets/scripts/playerCollision.cs
+++ EscapeTheForest/Assets/scripts/playerCollision.cs
@@ -5,16 +5,59 @@ using UnityEngine.SceneManagement;
 
 public class playerCollision : MonoBehaviour {
 
+    int hitCount = 0;
+    float time = 0.2f;
+
+    void Update() 
+    {
+
+        Debug.Log(hitCount);
+    }
     
     private void OnCollisionEnter(Collision collision)
     {
+
         if (collision.gameObject.tag == "Enemy")
         {
             Debug.Log(1);
             SceneManager.LoadScene("GameScreen");
-
         }
 
+    }
+
+    private void OnTriggerExit(Collider trigger) 
+    {
+        if (trigger.gameObject.tag == "Obstacle")
+        {
+
+            InvokeRepeating("timer", 1, 1);
+            hitCount += 1;
+
+            if (hitCount >= 4)
+            {
+                SceneManager.LoadScene("GameScreen");
+            }
+        }
+    }
+
+    public void timer() 
+    {
+            time -= Time.deltaTime;
+        
+       if (time <= 0)
+        {
+            time = 0.2f;
+           GetComponent<playerMovement>().speed = 10f;
+           CancelInvoke("timer");
+           
+        }
+        else if (time < 0.2f)
+        {
+            GetComponent<playerMovement>().speed = 5f;
+
+        }
+      
+    
     }
 
 
